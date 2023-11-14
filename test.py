fed5fd2473b34acddd3e537e0da8389d15656d72
Umar-Azam <umerazam256@gmail.com>
2023-11-14 22:23:16 +0000

    testing file list

--- test.py
+++ test.py
@@ -1,9 +1,33 @@
+# %%
 import os
 import openai
-from openai import OpenAI
 
 # %%
-client = OpenAI()
-client.api_key = os.environ.get('OPENAI_API_KEY')
+# getting key from file in parent directory.
+key = ''
 
+# Reading the content of the file into a variable
+with open('../openai_key', 'r') as file:
+    key = file.read()
+
+os.environ['OPENAI_API_KEY'] = key
+
+# %%
+client = openai.OpenAI()
 print(f'Key is {client.api_key}')
+
+# %%
+client.files.list()
+
+# %%
+
+
+def test_func(input1, input2, input3):
+    print(f'input1 : {input1}\ninput2 : {input2}\ninput3 : {input3}')
+
+
+# %%
+test_input = {'input1': 'hello', 'input2': 'world', 'input_3': '!'}
+# %%
+test_func(**test_input)
+# %%
